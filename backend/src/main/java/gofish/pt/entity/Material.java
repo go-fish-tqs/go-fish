4f9c7f8dbfe948790a479ed2353f32f31f7656c0
Tiago Oliveira <151779706+tiago-bd-oliveira@users.noreply.github.com>
2025-12-01 11:18:24 +0000

    GF 44 Search item by name (#11)

--- backend/src/main/java/gofish/pt/entity/Material.java
+++ backend/src/main/java/gofish/pt/entity/Material.java
@@ -1,61 +1,71 @@
 package gofish.pt.entity;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
 public enum Material {
 
     // --- Rod Materials ---
-    GRAPHITE(MaterialGroup.ROD_MATERIALS, "Graphite"),
-    FIBERGLASS(MaterialGroup.ROD_MATERIALS, "Fiberglass"),
-    COMPOSITE(MaterialGroup.ROD_MATERIALS, "Composite Blend"),
-    CARBON_FIBER(MaterialGroup.ROD_MATERIALS, "Carbon Fiber"),
+    GRAPHITE(MaterialGroup.RODS, "Graphite"),
+    FIBERGLASS(MaterialGroup.RODS, "Fiberglass"),
+    COMPOSITE(MaterialGroup.RODS, "Composite Blend"),
+    CARBON_FIBER(MaterialGroup.RODS, "Carbon Fiber"),
 
     // --- Reel Materials ---
-    ALUMINUM(MaterialGroup.REEL_MATERIALS, "Aluminum"),
-    MACHINED_ALUMINUM(MaterialGroup.REEL_MATERIALS, "Machined Aluminum"),
-    DIE_CAST_ALUMINUM(MaterialGroup.REEL_MATERIALS, "Die-Cast Aluminum"),
-    STAINLESS_STEEL(MaterialGroup.REEL_MATERIALS, "Stainless Steel"),
-    MAGNESIUM(MaterialGroup.REEL_MATERIALS, "Magnesium"),
-    BRASS(MaterialGroup.REEL_MATERIALS, "Brass"),
-    CARBON_COMPOSITE(MaterialGroup.REEL_MATERIALS, "Carbon Composite"),
-    POLYMER(MaterialGroup.REEL_MATERIALS, "Polymer"),
+    ALUMINUM(MaterialGroup.REELS, "Aluminum"),
+    MACHINED_ALUMINUM(MaterialGroup.REELS, "Machined Aluminum"),
+    DIE_CAST_ALUMINUM(MaterialGroup.REELS, "Die-Cast Aluminum"),
+    STAINLESS_STEEL(MaterialGroup.REELS, "Stainless Steel"),
+    MAGNESIUM(MaterialGroup.REELS, "Magnesium"),
+    BRASS(MaterialGroup.REELS, "Brass"),
+    CARBON_COMPOSITE(MaterialGroup.REELS, "Carbon Composite"),
+    POLYMER(MaterialGroup.REELS, "Polymer"),
 
     // --- Boats & Kayaks ---
-    ROTOMOLDED_POLYETHYLENE(MaterialGroup.BOAT_MATERIALS, "Rotomolded Polyethylene"),
-    THERMOFORMED_PLASTIC(MaterialGroup.BOAT_MATERIALS, "Thermoformed Plastic"),
-    INFLATABLE_PVC(MaterialGroup.BOAT_MATERIALS, "Inflatable PVC"),
-    HYPALON(MaterialGroup.BOAT_MATERIALS, "Hypalon"),
-    FIBERGLASS_BOAT(MaterialGroup.BOAT_MATERIALS, "Fiberglass"),
-    ALUMINUM_BOAT(MaterialGroup.BOAT_MATERIALS, "Aluminum Boat"),
+    ROTOMOLDED_POLYETHYLENE(MaterialGroup.BOATS, "Rotomolded Polyethylene"),
+    THERMOFORMED_PLASTIC(MaterialGroup.BOATS, "Thermoformed Plastic"),
+    INFLATABLE_PVC(MaterialGroup.BOATS, "Inflatable PVC"),
+    HYPALON(MaterialGroup.BOATS, "Hypalon"),
+    FIBERGLASS_BOAT(MaterialGroup.BOATS, "Fiberglass"),
+    ALUMINUM_BOAT(MaterialGroup.BOATS, "Aluminum Boat"),
 
     // --- Apparel & Wearables ---
-    NEOPRENE(MaterialGroup.APPAREL_MATERIALS, "Neoprene"),
-    BREATHABLE_FABRIC(MaterialGroup.APPAREL_MATERIALS, "Breathable Fabric"),
-    NYLON(MaterialGroup.APPAREL_MATERIALS, "Nylon"),
-    POLYESTER(MaterialGroup.APPAREL_MATERIALS, "Polyester"),
-    RUBBER(MaterialGroup.APPAREL_MATERIALS, "Rubber"),
-    PVC_COATED_FABRIC(MaterialGroup.APPAREL_MATERIALS, "PVC-Coated Fabric"),
+    NEOPRENE(MaterialGroup.APPARELS, "Neoprene"),
+    BREATHABLE_FABRIC(MaterialGroup.APPARELS, "Breathable Fabric"),
+    NYLON(MaterialGroup.APPARELS, "Nylon"),
+    POLYESTER(MaterialGroup.APPARELS, "Polyester"),
+    RUBBER(MaterialGroup.APPARELS, "Rubber"),
+    PVC_COATED_FABRIC(MaterialGroup.APPARELS, "PVC-Coated Fabric"),
 
     // --- Accessories / Tools ---
-    CARBON_STEEL(MaterialGroup.ACCESSORY_MATERIALS, "Carbon Steel"),
-    ANODIZED_ALUMINUM(MaterialGroup.ACCESSORY_MATERIALS, "Anodized Aluminum"),
-    TITANIUM(MaterialGroup.ACCESSORY_MATERIALS, "Titanium"),
-    HARD_PLASTIC(MaterialGroup.ACCESSORY_MATERIALS, "Hard Plastic"),
-    EVA_FOAM(MaterialGroup.ACCESSORY_MATERIALS, "EVA Foam"),
-    CORK(MaterialGroup.ACCESSORY_MATERIALS, "Cork"),
-    COMPOSITE_HANDLE(MaterialGroup.ACCESSORY_MATERIALS, "Composite Handle"),
+    CARBON_STEEL(MaterialGroup.ACCESSORIES, "Carbon Steel"),
+    ANODIZED_ALUMINUM(MaterialGroup.ACCESSORIES, "Anodized Aluminum"),
+    TITANIUM(MaterialGroup.ACCESSORIES, "Titanium"),
+    HARD_PLASTIC(MaterialGroup.ACCESSORIES, "Hard Plastic"),
+    EVA_FOAM(MaterialGroup.ACCESSORIES, "EVA Foam"),
+    CORK(MaterialGroup.ACCESSORIES, "Cork"),
+    COMPOSITE_HANDLE(MaterialGroup.ACCESSORIES, "Composite Handle"),
 
     // --- Nets ---
-    RUBBER_MESH(MaterialGroup.NET_MATERIALS, "Rubber Mesh"),
-    KNOTLESS_NYLON_MESH(MaterialGroup.NET_MATERIALS, "Knotless Nylon Mesh"),
-    ALUMINUM_FRAME(MaterialGroup.NET_MATERIALS, "Aluminum Frame"),
-    CARBON_FRAME(MaterialGroup.NET_MATERIALS, "Carbon Frame");
+    RUBBER_MESH(MaterialGroup.NETS, "Rubber Mesh"),
+    KNOTLESS_NYLON_MESH(MaterialGroup.NETS, "Knotless Nylon Mesh"),
+    ALUMINUM_FRAME(MaterialGroup.NETS, "Aluminum Frame"),
+    CARBON_FRAME(MaterialGroup.NETS, "Carbon Frame");
 
     public enum MaterialGroup {
-        ROD_MATERIALS,
-        REEL_MATERIALS,
-        BOAT_MATERIALS,
-        APPAREL_MATERIALS,
-        ACCESSORY_MATERIALS,
-        NET_MATERIALS
+        RODS,
+        REELS,
+        BOATS,
+        APPARELS,
+        ACCESSORIES,
+        NETS;
+
+        public List<Material> getMaterials() {
+            return Arrays.stream(Material.values()).filter(v -> v.getGroup() == this).toList();
+        }
     }
 
     private final MaterialGroup group;
@@ -65,13 +75,4 @@ public enum Material {
         this.group = group;
         this.displayName = displayName;
     }
-
-    public MaterialGroup getGroup() {
-        return group;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
 }
