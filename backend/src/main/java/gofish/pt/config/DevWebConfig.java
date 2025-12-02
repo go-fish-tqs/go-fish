4743c9153230e9b62e9936d03fe60570ab206d01
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-12-02 15:48:29 +0000

    GF-57-Register-new-item / help hotfix

--- backend/src/main/java/gofish/pt/config/DevWebConfig.java
+++ backend/src/main/java/gofish/pt/config/DevWebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Profile("dev")
-public class WebConfig {
+public class DevWebConfig {
 
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
