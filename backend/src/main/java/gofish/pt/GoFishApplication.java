494629cb4d6e05e492b2fcf0c0c738e94125e747
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-12-02 11:38:46 +0000

    GF-57-Register-new-item / Test correction

---
+++ backend/src/main/java/gofish/pt/GoFishApplication.java
@@ -0,0 +1,13 @@
+package gofish.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoFishApplication.class, args);
+    }
+
+}
